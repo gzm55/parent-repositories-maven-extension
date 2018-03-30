9539768b55ccfdd3f8e059acaf237fcac6c75fbe
James Z.M. Gao <gaozhiming@360.cn>
2018-03-30 18:57:03 +0800

    add normal reading test case

--- src/test/java/com/github/gzm55/maven/parent/BootRepositoriesReaderTest.java
+++ src/test/java/com/github/gzm55/maven/parent/BootRepositoriesReaderTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.apache.maven.model.Repository;
+import org.apache.maven.model.RepositoryPolicy;
 
 import org.codehaus.plexus.PlexusJUnit4TestCase;
 
@@ -21,14 +22,8 @@ import org.codehaus.plexus.PlexusJUnit4TestCase;
  */
 public class BootRepositoriesReaderTest extends PlexusJUnit4TestCase
 {
-  @BeforeEach
-  public void beforeTest() throws Exception {
-    super.beforeTest();
-  }
-  @AfterEach
-  public void afterTest() throws Exception {
-    super.afterTest();
-  }
+  @BeforeEach public void beforeTest() throws Exception { super.beforeTest(); }
+  @AfterEach  public void afterTest()  throws Exception { super.afterTest(); }
 
   @DisplayName("test null input")
   @Test
@@ -37,14 +32,113 @@ public class BootRepositoriesReaderTest extends PlexusJUnit4TestCase
     assertEquals("input file missing", e.getMessage());
   }
 
-  @DisplayName("test fake input")
+  @DisplayName("test empty input")
   @Test
-  void testFakeInput() throws IOException {
+  void testEmptyInput() throws IOException {
     final File file = new File(getClass().getClassLoader().getResource("empty.xml").getFile());
+
     final BootRepositoriesReader reader = lookup(BootRepositoriesReader.class);
     assertNotNull(reader);
+
     final List<Repository> l = reader.read(file);
     assertNotNull(l);
-    assertIterableEquals(new ArrayList<Repository>(0), l);
+
+    assertEquals(0, l.size());
+  }
+
+  @DisplayName("test normal input")
+  @Test
+  void testNormalInput() throws IOException {
+    final File file = new File(getClass().getClassLoader().getResource("normal.xml").getFile());
+    final List<Repository> l = lookup(BootRepositoriesReader.class).read(file);
+    assertNotNull(l);
+    assertEquals(3, l.size());
+
+    /**
+<repositories>
+  <repository>
+    <id>idA</id>
+    <name>nameA</name>
+    <url>urlA</url>
+  </repository>
+  <repository>
+    <id>idB</id>
+    <name>nameB</name>
+    <url>urlB</url>
+    <releases>
+      <updatePolicy>never</updatePolicy>
+    </releases>
+    <snapshots>
+      <enabled>false</enabled>
+    </snapshots>
+  </repository>
+  <repository>
+    <id>idC</id>
+    <name>nameC</name>
+    <url>urlC</url>
+    <releases>
+      <enabled>true</enabled>  
+      <updatePolicy>always</updatePolicy>  
+      <checksumPolicy>warn</checksumPolicy>
+    </releases>
+  </repository>
+</repositories>
+     */
+    final ArrayList<Repository> expected = new ArrayList<Repository>(3);
+
+    final Repository a = new Repository();
+    a.setId("idA");
+    a.setName("nameA");
+    a.setUrl("urlA");
+
+    final Repository b = new Repository();
+    b.setId("idB");
+    b.setName("nameB");
+    b.setUrl("urlB");
+    final RepositoryPolicy bRel = new RepositoryPolicy();
+    bRel.setUpdatePolicy("never");
+    final RepositoryPolicy bSnap = new RepositoryPolicy();
+    bSnap.setEnabled("false");
+    b.setReleases(bRel);
+    b.setSnapshots(bSnap);
+
+    final Repository c = new Repository();
+    c.setId("idC");
+    c.setName("nameC");
+    c.setUrl("urlC");
+    final RepositoryPolicy cRel = new RepositoryPolicy();
+    cRel.setEnabled("true");
+    cRel.setUpdatePolicy("always");
+    cRel.setChecksumPolicy("warn");
+    c.setReleases(cRel);
+
+    assertRepositoryEquals(a, l.get(0));
+    assertRepositoryEquals(b, l.get(1));
+    assertRepositoryEquals(c, l.get(2));
+
+    //expected.add(a);
+    //expected.add(b);
+    //expected.add(c);
+    //assertIterableEquals(expected, l);
+  }
+
+  private void assertRepositoryEquals(final Repository e, final Repository r) {
+    assertEquals(e.getId(), r.getId());
+    assertEquals(e.getName(), r.getName());
+    assertEquals(e.getUrl(), r.getUrl());
+    if (null == e.getReleases()) {
+      assertNull(r.getReleases());
+    } else {
+      assertEquals(e.getReleases().getEnabled(), r.getReleases().getEnabled());
+      assertEquals(e.getReleases().getUpdatePolicy(), r.getReleases().getUpdatePolicy());
+      assertEquals(e.getReleases().getChecksumPolicy(), r.getReleases().getChecksumPolicy());
+    }
+    if (null == e.getSnapshots()) {
+      assertNull(r.getSnapshots());
+    } else {
+      assertEquals(e.getSnapshots().getEnabled(), r.getSnapshots().getEnabled());
+      assertEquals(e.getSnapshots().getUpdatePolicy(), r.getSnapshots().getUpdatePolicy());
+      assertEquals(e.getSnapshots().getChecksumPolicy(), r.getSnapshots().getChecksumPolicy());
+    }
   }
 }
